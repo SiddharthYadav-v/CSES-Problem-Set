2256f077f03379c83ae40bf86fe73896cdedbe7a
Siddharth Yadav <siddharth0208yadav@gmail.com>
2023-08-18 00:48:03 +0530

    18-08-23 00:47

--- Dynamic Programming/coinCombinationsI.cpp
+++ Dynamic Programming/coinCombinationsI.cpp
@@ -4,26 +4,26 @@ using namespace std;
 #define int long long
 
 signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int n, x;
     cin >> n >> x;
 
     vector<int> coins(n);
     for(int i=0; i<n; i++) cin >> coins[i];
 
-    vector<int> dp(x+1, 0);
+    int mod=1e9+7;
+    vector<int> dp(x+1, -1);
+    dp[0]=1;
     for(int i=1; i<=x; i++) {
-        dp[i]=INT_MAX;
+        dp[i]=0;
         for(int j=0; j<n; j++) {
             if(i-coins[j]>=0) {
-                dp[i]=min(dp[i], dp[i-coins[j]]+1);
+                dp[i]+=dp[i-coins[j]]%mod;
+                dp[i]%=mod;
             }
         }
     }
 
-    cout << (dp[x]>=INT_MAX ? -1 : dp[x]);
+    cout << dp[x];
 
     // return 1;
 }
